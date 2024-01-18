adaa60948c398cc9a14c5621117390351ac9d7d4
alexjg <alex@memoryandthought.me>
2024-01-18 10:01:31 +0000

    Add methods to serialize cursors (#14)

--- lib/src/main/java/org/automerge/Cursor.java
+++ lib/src/main/java/org/automerge/Cursor.java
@@ -2,4 +2,21 @@ package org.automerge;
 
 public class Cursor {
 	private byte[] raw;
+
+	public static Cursor fromBytes(byte[] encoded) {
+		return AutomergeSys.cursorFromBytes(encoded);
+	}
+
+	public static Cursor fromString(String encoded) {
+		return AutomergeSys.cursorFromString(encoded);
+	}
+
+	@Override
+	public String toString() {
+		return AutomergeSys.cursorToString(this);
+	}
+
+	public byte[] toBytes() {
+		return raw.clone();
+	}
 }
